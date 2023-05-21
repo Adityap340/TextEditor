04fb7326b3d2506dca0815e59b74c6f22bde6555
Adityap340 <aditya_patil_comp@moderncoe.edu.in>
2023-05-21 21:57:38 +0530

    Aditional Features added

--- TextEditor.py
+++ TextEditor.py
@@ -7,6 +7,7 @@ from nltk import word_tokenize
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
 
+
 class TextEditor:
     def __init__(self, root):
         self.root = root
@@ -32,13 +33,14 @@ class TextEditor:
         self.format_menu.add_separator()
         self.format_menu.add_command(label="Bold", command=self.set_bold)
         self.format_menu.add_command(label="Italic", command=self.set_italic)
+        self.format_menu.add_command(label="Underline", command=self.set_underline)
         self.format_menu.add_separator()
         self.format_menu.add_command(label="Increase Font Size", command=self.increase_font_size)
         self.format_menu.add_command(label="Decrease Font Size", command=self.decrease_font_size)
         self.menu_bar.add_cascade(label="Format", menu=self.format_menu)
 
         # Initialize the current font
-        self.current_font = font.Font(family="Arial", size=12, weight="normal", slant="roman")
+        self.current_font = font.Font(family="Arial", size=12, weight="normal", slant="roman", underline=0)
 
     def open_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("Text Files", "*.txt")])
@@ -56,33 +58,40 @@ class TextEditor:
                 file.write(content)
 
     def set_font(self):
-        font_dialog = font.FontDialog(self.root)
-        new_font = font_dialog.show()
+        font_dialog = tk.font.Font(family=self.current_font.actual("family"), size=self.current_font.actual("size"))
+        new_font = font_dialog.askfont(self.root)
         if new_font:
-            self.current_font = new_font
-            self.text_widget.configure(font=new_font)
+            self.current_font.configure(family=new_font["family"], size=new_font["size"], weight=new_font["weight"],
+                                         slant=new_font["slant"], underline=new_font["underline"])
+            self.text_widget.configure(font=self.current_font)
 
     def set_bold(self):
-        bold = "bold" if self.current_font.actual()["weight"] != "bold" else "normal"
+        bold = "bold" if self.current_font.actual("weight") != "bold" else "normal"
         self.current_font.configure(weight=bold)
         self.text_widget.configure(font=self.current_font)
 
     def set_italic(self):
-        italic = "italic" if self.current_font.actual()["slant"] != "italic" else "roman"
+        italic = "italic" if self.current_font.actual("slant") != "italic" else "roman"
         self.current_font.configure(slant=italic)
         self.text_widget.configure(font=self.current_font)
 
+    def set_underline(self):
+        underline = 1 - self.current_font.actual("underline")
+        self.current_font.configure(underline=underline)
+        self.text_widget.configure(font=self.current_font)
+
     def increase_font_size(self):
-        size = self.current_font.actual()["size"]
+        size = self.current_font.actual("size")
         new_size = size + 2
         self.current_font.configure(size=new_size)
         self.text_widget.configure(font=self.current_font)
 
     def decrease_font_size(self):
-        size = self.current_font.actual()["size"]
+        size = self.current_font.actual("size")
         new_size = max(size - 2, 8)  # Ensure minimum font size of 8
         self.current_font.configure(size=new_size)
         self.text_widget.configure(font=self.current_font)
+
 
 if __name__ == "__main__":
     nltk.download("punkt")
